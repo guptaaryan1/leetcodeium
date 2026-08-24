9fc20ccb299904888bacfd0be78369e2ceeac750
guptaaryan1 <70092415+guptaaryan1@users.noreply.github.com>
2026-08-24 06:54:40 +0530

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,54 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False
+
+        # set_s1 = set(s1)
+        matches = 0
+        freq1 = [0] * 26
+        freq2 = [0] * 26
+        
+        for i in range(len(s1)):
+            freq1[ord(s1[i]) - ord('a')] += 1
+            freq2[ord(s2[i]) - ord('a')] += 1
+        for i in range(26):
+            if freq1[i] == freq2[i]:
+                matches += 1
+        
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            # freq2[ord(s2[i]) - ord('a')] += 1
+            index = ord(s2[r]) - ord('a')
+            freq2[index] += 1
+            if freq1[index] == freq2[index]:
+                matches += 1
+            elif freq1[index] + 1 == freq2[index]:
+                matches -= 1
+
+
+
+
+
+
+            index = ord(s2[l]) - ord('a')
+            freq2[index] -= 1
+            if freq1[index] == freq2[index]:
+                matches += 1
+            elif freq1[index] - 1 == freq2[index]:
+                matches -=1
+            # if s2[i] in set_s1:
+            #     matches += 1
+            # else:
+            #     matches -= 1
+            l += 1
+
+            
+
+        # abc - lec 
+        
+
+
+        return matches == 26
