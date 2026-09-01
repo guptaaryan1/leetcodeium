f4a132483f54986b5dd3b8111ec578c2796c24b1
guptaaryan1 <70092415+guptaaryan1@users.noreply.github.com>
2026-09-01 09:04:34 +0530

    Add: pacific-atlantic-water-flow - submission-4

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        def dfs(r, c, visited):
+            if (r >= rows or c >= cols or r < 0 or c < 0 or (r, c) in visited):
+                return False, False
+            visited.add((r, c))
+            pacific = r == 0 or c == 0
+            atlantic = r == rows - 1 or c == cols - 1
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+                if (0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] <= heights[r][c]):
+                    p, a = dfs(nr, nc, visited)
+                    pacific = p or pacific
+                    atlantic = a or atlantic
+            return pacific, atlantic
+        res = []
+
+        for r  in range(rows):
+            for c in range(cols):
+                p, a = dfs(r, c, set())
+                if p and a:
+                    res.append([r, c])
+        return res
+            
