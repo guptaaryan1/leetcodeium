302b23c4a86aa698eb529b6611602a0f233368f7
guptaaryan1 <70092415+guptaaryan1@users.noreply.github.com>
2026-09-07 02:40:04 +0530

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one = 1
+        two = 1
+        for i in range(2, n + 1):
+            one, two = two, one + two
+        return two
+            
