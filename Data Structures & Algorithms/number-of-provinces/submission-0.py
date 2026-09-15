38e5023ffa40c540183156b05df17e76d73f5d45
guptaaryan1 <70092415+guptaaryan1@users.noreply.github.com>
2026-09-15 13:16:54 -0500

    Add: number-of-provinces - submission-0

---
+++ Data Structures & Algorithms/number-of-provinces/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        class DisjointSet:
+            def __init__(self, n) -> None:
+                self.rank = [0] * (n + 1)
+                self.parent = [i for i in range(n + 1)]
+                pass
+            def findParent(self, node):
+                if self.parent[node] == node:
+                    return node
+                self.parent[node] = self.findParent(self.parent[node])
+                return self.parent[node]
+            def union(self, u, v):
+                upu = self.findParent(u)
+                upv = self.findParent(v)
+                if self.rank[upu] > self.rank[upv]:
+                    self.parent[upv] = upu
+                elif self.rank[upu] < self.rank[upv]:
+                    self.parent[upu] = upv
+                else:
+                    self.parent[upu] = upv
+                    self.rank[upv] += 1
+        n = len(isConnected)
+        ds = DisjointSet(n + 1)
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    ds.union(i, j)
+        roots = set()
+        for i in range(n):
+            roots.add(ds.findParent(i))
+        return len(roots)
+
